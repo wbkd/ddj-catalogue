777cf350715675f87743fde9c72b05c9d42b786b
moklick <info@moritzklack.com>
2014-11-17 00:28:16 +0100

    delete unused files

--- app/scripts/config.js
+++ app/scripts/config.js
@@ -1,7 +1,5 @@
-config = {
+module.exports = {
 	apiUrl: 'http://localhost:1337/api/v1/projects',
 	imageUrl : 'http://localhost:1337/images/',
 	defaultImage : 'http://placehold.it/100x100'
-}
-
-module.exports = config;
+};
