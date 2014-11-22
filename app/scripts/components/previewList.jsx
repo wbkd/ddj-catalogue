eb9d73469cea2331553885164e4e8fa240e3d3b0
Christopher <chrtze@gmail.com>
2014-11-22 20:34:46 +0100

    add active filter bar

--- app/scripts/components/previewList.jsx
+++ app/scripts/components/previewList.jsx
@@ -3,8 +3,10 @@ var Preview = require('./preview.jsx');
 var FilterActions = require('../actions/filterActions');
 var PreviewActions = require('../actions/previewActions');
 var PreviewStore = require('../stores/previewStore');
+var FilterStore = require('../stores/filterStore');
 var Sorter = require('./sorter.jsx');
 var config = require('../config');
+var SelectedFilters = require('./selectedFilters.jsx');
 
 var PreviewList = React.createClass({
 
@@ -12,7 +14,8 @@ var PreviewList = React.createClass({
         return {
           previews : [],
           expandedId : null,
-          sorting: config.initialSorting
+          sorting: config.initialSorting,
+          selectedFilters: {},
         };
       },
 
@@ -23,6 +26,7 @@ var PreviewList = React.createClass({
 
       componentDidMount: function() {
         this.unsubscribe = PreviewStore.listen(this.onStatusChange);
+        FilterStore.listen(this.onStatusChange);
         //load initial previews
         PreviewActions.load();
       },
@@ -42,7 +46,6 @@ var PreviewList = React.createClass({
             sortBy: 'date'
           }
         ]
-
         var previews = this.state.previews.map(function(preview,i) {
           var isExpanded = this.state.expandedId ? this.state.expandedId === preview._id : false,
             isStared = this.props.favoriteIds.indexOf(preview._id) !== -1;
@@ -54,9 +57,11 @@ var PreviewList = React.createClass({
         return (
           <div className="preview-list row centered">
             <div className="clearfix preview-list-header">
-             
               <div onClick={this.toggleFilterMenu} className="btn btn-filter btn"><i className="icon_menu"></i> Liste filtern</div>
               <Sorter active={this.state.sorting} sorts={sorts}/>
+            </div>
+            <div className="selected-filters clearfix">
+             <SelectedFilters filters={this.state.selectedFilters} />
             </div>
             <div className="preview-list-content row">
               {previews}
---
+++ app/scripts/components/selectedFilters.jsx
@@ -0,0 +1,32 @@
+var React = require('react');
+var Filter = require('./filter.jsx');
+
+var SelectedFilters = React.createClass({
+	
+	getFilters: function() {
+		var filters = [];
+		for(var key in this.props.filters) {
+  			if (this.props.filters.hasOwnProperty(key)) {
+    			filters.push({
+    				category: key,
+    				text: this.props.filters[key],
+    				checked: true
+    			});
+  			}
+		}
+		return filters.map(function(filter, i) {
+      		return <Filter key={filter + i} text={filter.text} category={filter.category} checked={filter.checked}/>
+    	});
+	},
+
+	render: function() {
+
+		var filters = this.getFilters();
+
+		return (
+			<ul>{filters}</ul>
+		);
+	}
+});
+
+module.exports = SelectedFilters;
