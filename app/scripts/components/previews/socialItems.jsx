dd89d8fbceee4600c55bcb935b4aa91616ab5768
moklick <info@moritzklack.com>
2014-12-09 14:21:45 +0100

    udpate preview style/handling

--- app/scripts/components/previews/socialItems.jsx
+++ app/scripts/components/previews/socialItems.jsx
@@ -5,8 +5,9 @@ var SocialItems = React.createClass({
 
   render: function() {
 
-    if(utils.isUndefined(this.props.socialData)){
-      return false;
+    if(utils.isUndefined(this.props.socialData) || !this.props.socialData){
+      //return false;
+      this.props.socialData = { facebook : 12 , twitter : 12 }
     }
 
     return (
