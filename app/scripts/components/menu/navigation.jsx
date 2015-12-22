84c76eca82de95c6aba391bc0d85a2a612ce4c2d
moklick <info@moritzklack.com>
2015-12-22 14:09:49 +0100

    rename dacosto to lokaler, remove newsletter section

--- app/scripts/components/menu/navigation.jsx
+++ app/scripts/components/menu/navigation.jsx
@@ -8,7 +8,6 @@ var Navigation = React.createClass({
     return (
     	<nav className="clearfix">
         <span className="nav-item" onClick={ MenuActions.toggleSubmitArea }><i className="icon_upload"></i><span className="label">Einreichen</span></span>
-        <span className="nav-item" onClick={ MenuActions.toggleNewsletterArea }><i className="icon_mail_alt"></i><span className="label">Newsletter</span></span>
         <span className="nav-item" onClick={ MenuActions.toggleFaqArea }><i className="icon_question"></i><span className="label">FAQ</span></span>
         <span className="nav-item" onClick={ MenuActions.toggleInfo }><i className="icon_info_alt"></i><span className="label">Info</span></span>
       </nav>
